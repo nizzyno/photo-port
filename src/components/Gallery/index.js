1e04f1bd03a84bb79416c3a53699668a7acffb80
Noah <mcbeasty11.nh@icloud.com>
2021-06-27 08:26:46 -0500

    gallery images rendered

---
+++ src/components/Gallery/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PhotoList from '../PhotoList';
+import { capitalizeFirstLetter } from '../../utils/helpers';
+
+function Gallery(props) {
+  const { currentCategory } = props;
+  return (
+    <section>
+      <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
+      <p>{currentCategory.description}</p>
+      <PhotoList category={currentCategory.name} />
+    </section>
+  );
+}
+
+export default Gallery;
